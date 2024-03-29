f8da9b27b2036179651bc3adf6e80e9fc90d5c7a
KYUTAE PARK <rbxo0109@gmail.com>
2024-03-29 23:21:49 +0900

    feat: 코드 정리

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/link"
+
 import MyPageSidebarForm from "@/components/mypagesidebar/MyPageSidebarForm";
 import MyPageInfoForm from "@/components/mypageinfo/MyPageInfo";
 
